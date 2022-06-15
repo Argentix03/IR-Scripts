0171d97d52875440f5bc3ce78c81adfa25f1498b
ghosts621 <97026636+ghosts621@users.noreply.github.com>
2022-06-15 08:58:16 +0300

    Update Enabled_Monitoring_On_Registry_Keys.ps1

--- Registry Auditing Automator/Enabled_Monitoring_On_Registry_Keys.ps1
+++ Registry Auditing Automator/Enabled_Monitoring_On_Registry_Keys.ps1
@@ -4,6 +4,7 @@ try{
 catch [System.SystemException]{ write-host "High Permissions Required! Re-Run the script with a privileged user..." -ForegroundColor DarkRed }
 $count_Success=0
 $count_False=0
+$flag=0
 foreach($line in Get-Content registry.txt) {
 	$linewithouspasificstring=$line -replace ":",""
 	"REG QUERY $linewithouspasificstring | $line" | Out-File -FilePath Test_Registry_Key.txt -Append
@@ -14,13 +15,15 @@ foreach($line in Get-Content Test_Registry_Key.txt) {
 	$a=Invoke-Expression $fertparm_for_testing_registry_key 
 	echo $a.Length
 	if ( $a.Length -eq 0 ){
+		$count_False+=1
+		$flag=1
 		write-host "Error system was unable to find the specified registry key or value $line" -ForegroundColor red
 		"$fertparm_for_testing_registry_key" | Out-File -FilePath Error.txt -Append
-		$count_False+=1
+		
 	}else{
 		write-host "$line" -ForegroundColor Green
+		$count_Success +=1
 		$secdparm_to_enable__registry_key.remove(0, 1) | Out-File -FilePath OK.txt -Append
-		$count_Secsess +=1
 	}
 }
 clear
@@ -46,16 +49,22 @@ foreach($line in Get-Content OK.txt) {
 }
 clear
 write-host "The script Success to enable monitoring on $count_Success registry Key" -ForegroundColor green 
-write-host "The script Faile to enable monitoring on $count_False registry Key" -ForegroundColor RED 
+write-host "The script Faile to enable monitoring on $count_False registry Key chake the Error.txt file" -ForegroundColor RED 
 $FileToDelete1= "Test_Registry_Key.txt"
 $FileToDelete2 = "OK.txt"
 $FileToDelete3 = "Error.txt"
 if (Test-Path -Path $FileToDelete1) {
-	Remove-Item -Path $FileToDelete
+	Remove-Item -Path $FileToDelete1
+}else{
+	
 }
 if (Test-Path -Path $FileToDelete2) {
 	Remove-Item -Path $FileToDelete2
+}else{
+	
 }
-if (Test-Path -Path $FileToDelete3) {
-	Remove-Item -Path $FileToDelete2
+if ((Test-Path -Path $FileToDelete3) -and ($flag -eq 1)) {
+	Remove-Item -Path $FileToDelete3
+}else{
+	
 }
